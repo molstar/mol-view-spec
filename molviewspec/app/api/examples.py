d7a38d85a43c85e467a65fa967e8412b59652e99
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2023-05-25 15:15:53 -0700

    desc

--- molviewspec/app/api/examples.py
+++ molviewspec/app/api/examples.py
@@ -7,11 +7,11 @@ from molviewspec.builder import Root
 router = APIRouter()
 
 
-@router.get("/load/{id}",
-            description="""
-Download a minimal example that visualizes a given PDB entry in cartoon representation.
-            """)
+@router.get("/load/{id}")
 async def download_example(id: str):
+    """
+    Download a minimal example that visualizes a given PDB entry in cartoon representation.
+    """
     builder = Root()
     (
         builder.download(url=f"https://www.ebi.ac.uk/pdbe/entry-files/download/{id.lower()}_updated.cif")
@@ -25,20 +25,27 @@ async def download_example(id: str):
 
 @router.get("/label/{id}")
 async def label_example(id: str):
+    """
+    The minimal example enriched by custom labels and labels read from the CIF source file.
+    """
     builder = Root()
-    (
+    structure = (
         builder.download(url=f"https://www.ebi.ac.uk/pdbe/entry-files/download/{id.lower()}_updated.cif")
         .parse(format="mmcif")
         .structure()
-        .label(label_asym_id="A", label_seq_id=120, text="Residue 1")
-        .label(label_asym_id="C", label_seq_id=271, text="Residue 2")
-        .label_from_cif(cif_category_name="my_custom_cif_category")
     )
+    structure.component().representation()
+    structure.label(label_asym_id="A", label_seq_id=120, text="Residue 1")\
+        .label(label_asym_id="C", label_seq_id=271, text="Residue 2")\
+        .label_from_cif(cif_category_name="my_custom_cif_category")
     return JSONResponse(builder.node)
 
 
 @router.get("/color/{id}")
 async def color_example(id: str):
+    """
+    An example with different representations and coloring for polymer and non-polymer chains.
+    """
     builder = Root()
     structure = (
         builder.download(url=f"https://www.ebi.ac.uk/pdbe/entry-files/download/{id.lower()}_updated.cif")
@@ -56,18 +63,27 @@ async def color_example(id: str):
 
 @router.get("/data/{id}/molecule")
 async def cif_data_molecule(id: str):
+    """
+    Download the content of `molecule.cif`.
+    """
     path = settings.TEST_DATA_DIR / id / "molecule.cif"
     return FileResponse(path)
 
 
 @router.get("/data/{id}/cif-annotations")
 async def cif_data_annotation(id: str):
+    """
+    Download the content of `annotations.cif`.
+    """
     annotations = (settings.TEST_DATA_DIR / id / "annotations.cif").read_text()
     return PlainTextResponse(f"data_{id}_annotations\n{annotations}")
 
 
 @router.get("/data/{id}/molecule-and-cif-annotations")
 async def cif_data_molecule_and_annotation(id: str):
+    """
+    Get a mmCIF structure file with the contents of `annotations.cif` concatenated to the end.
+    """
     mol = (settings.TEST_DATA_DIR / id / "molecule.cif").read_text()
     annotations = (settings.TEST_DATA_DIR / id / "annotations.cif").read_text()
     return PlainTextResponse(f"{mol}\n\n{annotations}")
@@ -76,7 +92,7 @@ async def cif_data_molecule_and_annotation(id: str):
 @router.get("/data/{id}/json-annotations")
 async def json_list(id: str):
     """
-    Lists all available JSON annotations
+    Lists all available JSON annotations for a given `id`.
     """
     path = settings.TEST_DATA_DIR / id
     names = [f.name[:-5] for f in path.glob("*.json")]
@@ -85,5 +101,8 @@ async def json_list(id: str):
 
 @router.get("/data/{id}/json/{name}")
 async def json_data(id: str, name: str):
+    """
+    Download a specific JSON file. Use the `data/{id}/json-annotations` endpoint to discover available files.
+    """
     path = settings.TEST_DATA_DIR / id / f"{name}.json"
     return FileResponse(path)
