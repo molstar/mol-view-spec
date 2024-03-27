cad629407efe200b1f54e99d044aab1563313fb5
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2024-03-27 15:47:26 -0700

    version

--- molviewspec/molviewspec/__init__.py
+++ molviewspec/molviewspec/__init__.py
@@ -1,6 +1,6 @@
 """MolViewSpec"""
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 from molviewspec.builder import create_builder
 from molviewspec.nodes import ComponentExpression, validate_state_tree
