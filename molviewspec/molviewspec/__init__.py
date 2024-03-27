4a218598849089ab2d114a8c0f5e8b778fdcbe8d
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2024-03-27 15:56:08 -0700

    README ref

--- molviewspec/molviewspec/__init__.py
+++ molviewspec/molviewspec/__init__.py
@@ -1,6 +1,6 @@
 """MolViewSpec"""
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 from molviewspec.builder import create_builder
 from molviewspec.nodes import ComponentExpression, validate_state_tree
