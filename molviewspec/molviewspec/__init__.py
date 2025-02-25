17a7f1431dbc72e96bff57fce7114e9296ee7d6a
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2025-02-25 09:34:34 -0800

    prepare release

--- molviewspec/molviewspec/__init__.py
+++ molviewspec/molviewspec/__init__.py
@@ -1,6 +1,6 @@
 """MolViewSpec"""
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 from molviewspec.builder import create_builder
 from molviewspec.nodes import ComponentExpression, validate_state_tree
