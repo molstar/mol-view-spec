f177dee2c413106aa4312313d00d99262b411287
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2025-02-26 16:54:29 -0800

    export state-related nodes

--- molviewspec/molviewspec/__init__.py
+++ molviewspec/molviewspec/__init__.py
@@ -3,4 +3,13 @@
 __version__ = "1.2.0"
 
 from molviewspec.builder import create_builder
-from molviewspec.nodes import ComponentExpression, validate_state_tree
+from molviewspec.nodes import (
+    ComponentExpression,
+    GlobalMetadata,
+    PrimitiveComponentExpressions,
+    Snapshot,
+    SnapshotMetadata,
+    State,
+    States,
+    validate_state_tree,
+)
