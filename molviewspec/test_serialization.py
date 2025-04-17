8f320f66aa4d6ad21cb4b1149d40f5881b45cd07
Jochem Smit <jhsmit@gmail.com>
2025-04-17 11:05:12 +0200

    refactor: clean up test module for builder serialization

--- molviewspec/test_serialization.py
+++ molviewspec/test_serialization.py
@@ -1,40 +1,32 @@
-"""Test module for converting builder to msvj state."""
+"""Test module builder serialization."""
 
-import json
 import logging
-import os
-import shutil
-import tempfile
 import unittest
-import urllib.request
-import zipfile
-from urllib.parse import urlparse
 
 from molviewspec.builder import create_builder
 
 # Set up logging for tests
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
 
 
-
 class TestSerialization(unittest.TestCase):
-
     def test_box(self):
         """Test box serialization."""
         builder = create_builder()
         builder.primitives().box(
             center=(0.5, 0.5, 1.0),
-            extent=(0.5, 0.5, 2.),  
+            extent=(0.5, 0.5, 2.0),
             show_faces=True,
             face_color="blue",
             show_edges=False,
         )
         state = builder.get_state()
-        
+
         self.assertIn("primitives", state)
         self.assertIn("box", state)
-
 
 
 if __name__ == "__main__":
